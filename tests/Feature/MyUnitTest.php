745f6e7e7fbe28341568d187afe0fe514182f163
suresh <sureshkushmi@gmail.com>
2024-07-05 15:35:00 +0545

    Add README.md

---
+++ tests/Feature/MyUnitTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class MyUnitTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $result = 2 + 2;
+        $this->assertEquals(4, $result);
+    }
+
+    
+}
